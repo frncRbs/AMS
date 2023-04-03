cf06f7bd351dad093164096d1d862baca8a29bb2
France Rebollos <frncerebollos@gmail.com>
2023-04-03 23:47:28 +0800

    Note To Jhomz Pogi

--- controller/admin/delete_farmer_request.php
+++ controller/admin/delete_farmer_request.php
@@ -30,7 +30,9 @@
     }
     $database->close();
 
+    // Pwede tu ki mn append el sms code after el delete query nuay pa iyo mn add table para donde mn insert el message etu ylng ase
     
+
     
     $return_dict = [
         'status' => $return_value,
